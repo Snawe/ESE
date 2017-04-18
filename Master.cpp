39385c6734f777a5fa0a2b6367b378c826018824
Alexander <alex_haubner@yahoo.de>
2017-04-18 13:13:01 +0200

    After Cleanup and Error Fixing + getting rid of warnings + Gitignore images

--- Master.cpp
+++ Master.cpp
@@ -102,85 +102,93 @@ TODO:
 #include <stdio.h>
 //#include <iostream>
 //#include<conio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
 
 // An unsigned char can store 1 Bytes (8bits) of data (0-255)
-typedef unsigned char BYTE;
-void printImg(long fileSize, BYTE *fileBuf, int width);
-void convert(BYTE *frmBuffer);
-void getKoordinates(long fileSize, BYTE *fileBuf);
-int calcStat(long fileSize, BYTE *fileBufOld, BYTE *fileBufNew, int offset);
-
-struct metadata{
-	int width;
-	int height;
-	int color;
-}metad;
-
-//struct statistic {
-//	int N;
-//	int average;
-//	int variance;
-//	int stddev;
-//	int old;
-//}statistic;
-
-typedef struct statistic {
+typedef struct stats {
 	int N;
 	int average;
 	int variance;
-	int stddev;
+	double stddev;
 	int old;
 } statistic;
 
+struct metadata {
+	int width;
+	int height;
+	int color;
+};
+
+typedef unsigned char BYTE; 
+long getFileSize(FILE *file);
+int metadata(FILE *fp);
+int rollingStatistic(int val, statistic *stat);
+
+const char *filePath[20] = {
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\11.pnm",
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" ,
+	"testM\\1.pnm" };
+	//"testM\\2.pnm" ,
+	//"testM\\3.pnm" ,
+	//"testM\\4.pnm" ,
+	//"testM\\5.pnm" ,
+	//"testM\\6.pnm" ,
+	//"testM\\7.pnm" ,
+	//"testM\\8.pnm" ,
+	//"testM\\9.pnm" ,
+	//"testM\\10.pnm" ,
+	//"testM\\11.pnm" ,
+	//"testM\\12.pnm" ,
+	//"testM\\13.pnm",
+	//"testM\\14.pnm",
+	//"testM\\15.pnm" };
+
+const char *filePathChange[20] = {
+	"testM\\_01.pnm" ,
+	"testM\\_02.pnm" ,
+	"testM\\_03.pnm" ,
+	"testM\\_04.pnm" ,
+	"testM\\_05.pnm" ,
+	"testM\\_06.pnm" ,
+	"testM\\_07.pnm" ,
+	"testM\\_08.pnm" ,
+	"testM\\_09.pnm" ,
+	"testM\\_10.pnm" ,
+	"testM\\_11.pnm" ,
+	"testM\\_12.pnm" ,
+	"testM\\_13.pnm",
+	"testM\\_14.pnm",
+	"testM\\_16.pnm",
+	"testM\\_17.pnm" ,
+	"testM\\_18.pnm" ,
+	"testM\\_19.pnm" ,
+	"testM\\_20.pnm" ,
+	"testM\\_21.pnm" };
+
+statistic stat[64000];
+
 int main()
 {
-	int lengh = 0;
-	double dif = 0;
-	int width = 30;
-
-	const char *filePath[15] = {
-		"testM\\1.pnm" ,
-		"testM\\2.pnm" ,
-		"testM\\3.pnm" ,
-		"testM\\4.pnm" ,
-		"testM\\5.pnm" ,
-		"testM\\6.pnm" ,
-		"testM\\7.pnm" ,
-		"testM\\8.pnm" ,
-		"testM\\9.pnm" ,
-		"testM\\10.pnm" ,
-		"testM\\11.pnm" ,
-		"testM\\12.pnm" ,
-		"testM\\13.pnm",
-		"testM\\14.pnm", 
-		"testM\\15.pnm" };
-	const char *filePathChange[15] = {
-		"testM\\_131351633974511983.pnm" ,
-		"testM\\_131351633985001584.pnm" ,
-		"testM\\_131351633995532702.pnm" ,
-		"testM\\_131351634006337327.pnm" ,
-		"testM\\_131351634016712236.pnm" ,
-		"testM\\_131351634026567246.pnm" ,
-		"testM\\_131351634036888307.pnm" ,
-		"testM\\_131351634047768552.pnm" ,
-		"testM\\_131351634062397230.pnm" ,
-		"testM\\_131351634074522928.pnm" ,
-		"testM\\__131351634047768552.pnm" ,
-		"testM\\__131351634062397230.pnm" ,
-		"testM\\__131351634074522928.pnm",
-		"testM\\14.pnm", 
-		"testM\\15.pnm" };
-	// 2 Bilder einlesen und speichern in neuen
-	const char *filePathOld = "Grayscale_Static\\131351633974511983.pnm";
-	const char *filePathNew = "Grayscale_Static\\131351634006337327.pnm"; // Kopf
-	const char *filePathDif = "Grayscale_Static\\testa.pnm";
-
-	BYTE *fileBufOld;		// Pointer to our buffered data
-	BYTE *fileBufNew;		// Pointer to our buffered data
-	FILE *fpOld;			// File pointer
+	BYTE fileBufNew;		// Pointer to our buffered data
 	FILE *fpNew;			// File pointer
 	FILE *fpDif;			// File pointer für Dif im Bild
 
@@ -195,176 +203,76 @@ int main()
 	"w+"	Creates an empty file for both reading and writing.
 	"a+"	Opens a file for reading and appending.
 	*/
-	fpOld = fopen(filePathOld, "r");
-	fpNew = fopen(filePathNew, "r");
-	fpDif = fopen(filePathDif, "w");
 
 	// Get the size of the file in bytes
-	long fileSizeOld = getFileSize(fpOld);
-	long fileSizeNew = getFileSize(fpNew);
+	long fileSizeNew = 0;
+	int metad = 0;
 
-	// Allocate space in the buffer for the whole file	
-	// fileBuf = new BYTE[fileSize];
-	fileBufOld = (BYTE*)malloc(fileSizeOld); // umgeschrieben auf malloc
-	fileBufNew = (BYTE*)malloc(fileSizeNew); // umgeschrieben auf malloc
-
-	// Now that we have the entire file buffered, we can take a look at some binary infomation
-	// Lets take a look in hexadecimal
-	printf("BILD 1\n");
-	printf(" "); // Damit die Formatierung von Bild 1 und 2 gleich aussieht (NUR FÜR KONSOLE)
-
-	// Read the file in to the buffer
-	fread(fileBufOld, fileSizeOld, 1, fpOld);
-	if (DEBUG_PRINT)
-		printImg(fileSizeOld, fileBufOld, width);
-
-
-	printf("\n\nBILD 2\n ");
-	// Read the file in to the buffer
-	fread(fileBufNew, fileSizeNew, 1, fpNew);
-	if (DEBUG_PRINT)
-		printImg(fileSizeNew, fileBufNew, width);
-	// TODO: WTF???? Ich lese nur aus, schreibe sofort wieder rein und das bild schaut anders aus........
-	//
-	//fwrite(fileBufNew, fileSizeNew, 1, fpDif);
-
-	/*convert(fileBufNew);
-	convert(fileBufOld);*/
-
-	printf("\n\nBILDER VERGLEICH\n ");
-	// TODO: Unterschiedliche Länge hat sich (glaub ich) gelöst. Muss noch checken.
-	// Aus irgend einem Grund ist die FileSizeNew und FileSizeOld unterschiedlich
-	// Darum wird hier verglichen welche länger ist, und die längere genommen.
-	if (fileSizeOld >= fileSizeNew)
-		lengh = fileSizeOld;
-	else lengh = fileSizeNew;
-
-	for (int i = 0; i < lengh; i++) {
-		if (fileBufNew[i] == fileBufOld[i]) {
-			if (DEBUG_PRINT)
-				printf("   "); // 3 leere Zeichen um Formatierung zu halten
-			// fileBufNew[i] = fileBufOld[i] - fileBufNew[i];
-		}
-		else {
-			int j = fileBufOld[i] - fileBufNew[i]; // TODO: Voneinander Abziehen um den Hexa-Wert als unterschied zu bekommen
-													// Nicht unbedingt sinnhaft.
-			if (DEBUG_PRINT)
-				printf("%X ", j);
-			dif++;
-		}
-		if (i % width == 0 && i != 0)
-			if (DEBUG_PRINT)
-				printf("\n ");
+	for (int i = 0; i < 64000; i++) {
+		stat[i].N = 5;
+		stat[i].average = 128;
+		stat[i].variance = 200;
+		stat[i].stddev = sqrt(stat[i].variance);
+		stat[i].old = 128;
 	}
-   
-	int metad = metadata(fileBufNew);
-	dif = dif * 100 / (lengh - metad);
-	printf("\nProzentuelle Abweichung (Pixel gegen Pixel): %.2f %%\n", dif);
 
-	//int **sValue = (BYTE*)malloc(fileSizeNew * sizeof(int));// Rows
-	//for (int i = 0; i < fileSizeNew; i++) { //Colums
-	//	sValue[i] = malloc(2);
-	//}
-	
-	//for (int i = 0; i < 10; i++) {
-	//	double calcdif = calcStat(fileSizeNew, fileBufOld, fileBufNew, metad, sValue);
-	//	calcdif = calcdif * 100 / (lengh - metad);
-	//	printf("Prozentuelle Abweichung (Statistisch): %.2f %%\n", calcdif);
-	//}
-	/*statistic.N = 10;
-	statistic.average = 128;
-	statistic.variance = 128;
-	statistic.stddev = sqrt(statistic.variance);
-	int first = 128;
-	statistic.old = first;	
-	int val = fileBufNew;*/
-	int first = 128;
-	struct statistic statistic[50703];
-	double calcdif = 0;
-	
-	for (int j = 0; j < 13; j++) {
-		calcdif = 0;
-		fpNew = fopen(filePath[j], "r");
-		fpDif = fopen(filePathChange[j], "w");
+	for (int j = 0; j < (sizeof(filePath)/sizeof(filePath[0])); j++) {
+		fpNew = fopen(filePath[j], "rb");
+		fpDif = fopen(filePathChange[j], "wb");
+		metad = metadata(fpNew);
+		fileSizeNew = getFileSize(fpNew);
+		rewind(fpNew);
 		for (int i = 0; i < metad; i++) {
-			fread(fileBufNew, 1, 1, fpNew);
-			fwrite(fileBufNew, 1, 1, fpDif);
+			fread(&fileBufNew, 1, 1, fpNew);
+			fwrite(&fileBufNew, 1, 1, fpDif);
 		}
+		printf("%s\n", filePath[j]);
 		for (int i = metad; i < fileSizeNew; i++) {
-			fread(fileBufNew, 1, 1, fpNew);
-			rollingStatistic(j, i, fileBufNew[0], statistic);
-			/*if (fileBufNew[0] > (statistic[i].average + statistic[i].stddev * 3))
-				fileBufNew[0] = 0x00;
-			else if (fileBufNew[0] < (statistic[i].average + statistic[i].stddev * 3))
-				fileBufNew[0] = 0x00;*/
-			fileBufNew[0] = statistic[i].average;
-			int c = fwrite(fileBufNew, 1, 1, fpDif);
+			fread(&fileBufNew, 1, 1, fpNew);
+			rollingStatistic(fileBufNew, &(stat[i]));
+			
+			if (fileBufNew > (stat[i].average + stat[i].stddev * 3))
+				fileBufNew = 0xFF;
+			else if (fileBufNew < (stat[i].average - stat[i].stddev * 3))
+				fileBufNew = 0xFF;
+			else
+				fileBufNew = 0x00;
+
+			//fileBufNew = (BYTE)stat[i].average;
+			int c = fwrite(&fileBufNew, 1, 1, fpDif);
 			if(c != 1)
-				printf("%d", c);
-			//else 
-			//	fileBufNew[i] = 0xFF0000;
-			//if ((fileBufNew[i] > (statistic[i].average + statistic[i].stddev * 3)) || (fileBufNew[i] < (statistic[i].average + statistic[i].stddev * 3)))
-			//	fileBufNew[i] = 0xFF;
-			//else {
-			//	fileBufNew[i] = 0x00;
-			//	calcdif++;
-			//}
+				printf("%d", c);			
 		}		
-		calcdif = calcdif * 100 / (lengh - metad);
-		printf("Prozentuelle Abweichung (Statistisch): %.2f %%\n", calcdif);
 		fclose(fpDif);
 		fclose(fpNew);
-	}
-	//for (int o = metad; o < fileSizeNew; o++){
-	//	//if (fileBufNew[o] == EOF)
-	//	//	break;
-	//	/*if (o == 52)
-	//		o = 53;*/
-	//	char c = fileBufNew[o];
-	//	fprintf(fpDif, "%c", c);
-	//}
-	// fwrite(fileBufNew, fileSizeNew, 1, fpDif);
-	// getKoordinates(fileSizeNew, fileBufNew);
-
+	}	
 	if (DEBUG_WAIT) {
 		char wait = '0';
 		while (wait == '0') {
 			scanf("%c", &wait);
 		}
 	}
-	
-	/*cin.get();
-	delete[]fileBuf;*/
-	fclose(fpOld);
 	fclose(fpNew);
 	fclose(fpDif);
+
 	return 0;
 }
-int rollingStatistic(int j, int i, int val, statistic *statistic) {
-	// static int count = 0;
-	// struct statistic *statistic = (BYTE*)malloc(fileSizeNew);
-	if (j == 0) { // init 
-		statistic[i].N = 10;
-		statistic[i].average = 128;
-		statistic[i].variance = 128;
-		statistic[i].stddev = sqrt(statistic[i].variance);
-		statistic[i].old = 128;
-		// count++;
-	}
 
-	int oldavg = statistic[i].average;
-	int newavg = oldavg + (val - statistic[i].old) / statistic[i].N;
-	statistic[i].average = newavg;
+int rollingStatistic(int val, statistic *stat) {
+	int oldavg = stat->average;
+	int newavg = (oldavg - oldavg / stat->N) + (val / stat->N);
+	stat->average = newavg;
 	//( NEU - ATL ) * ( NEU - AVG + ALT - AltAVT
 	//int newvar = (abs(val - statistic[i].old))*(abs(val - newavg + statistic[i].old - oldavg)) / (statistic[i].N - 1);
-	int newvar = (val - statistic[i].old)*(val - newavg + statistic[i].old - oldavg) / (statistic[i].N - 1);
-	if ((statistic[i].variance + newvar) < 0)
-		newvar = statistic[i].variance;
-	statistic[i].variance = newvar;
+	// int newvar = (val - stat->old)*(val - newavg + stat->old - oldavg) / (stat->N - 1);
+	int newvar = stat->N * pow(newavg - oldavg, 2);
+	if ((stat->variance + newvar) < 0)
+		newvar = stat->variance;
+	stat->variance = newvar;
 	//print("V:" + str(self.variance))
 	/*printf("variance = %i\n", statistic.variance);*/
-	statistic[i].stddev = sqrt(statistic[i].variance);
-	statistic[i].old = val;
+	stat->stddev = sqrt(stat->variance);
+	stat->old = val;
 
 	//int oldavg = statistic[i].average;
 	//int newavg = oldavg + (val - statistic[i].old) / statistic[i].N;
@@ -375,71 +283,8 @@ int rollingStatistic(int j, int i, int val, statistic *statistic) {
 
 	//return statistic;
 	//count++;
-}
 
-int calcStat(long fileSize, BYTE *fileBufOld, BYTE *fileBufNew, int offset, int **sValue) {
-	static int count = 1;
-	int sigma = 20;
-	int my = 0;
-	int statDif = 0;
-	
-	if (count == 1) {	
-		// µ und sigma setzen. (erste Aufruf)
-		for (int i = 0; i < fileSize; i++) {
-			sValue[i][0] = fileBufOld[i]; // my = fileBufOld[i];	=> Beim Initialisieren wird µ als erster Wert des Pixels angenommen.
-			sValue[i][1] = sigma; // Std. Value von 25 (empirisch)
-		}
-
-		// CALCULATION
-		for (int i = 0; i < fileSize; i++) {
-			if ((fileBufNew[i] <= (sValue[i][0] - sValue[i][1])) || (fileBufNew[i] >= (sValue[i][0] + sValue[i][1]))) {
-				if (DEBUG_PRINT)
-					printf("fileBufNew %i ist größer oder kleiner µ +- sigma\n");
-				statDif++;
-				sValue[i][0] = changeMy(count, fileBufNew[i], sValue[i][0]);
-			}
-			//fileBufOld[i] = fileBufNew[i];
-		}
-	}
-	else {
-		// CALCULATION
-		for (int i = 1; i < fileSize; i++) {
-			if ((fileBufNew[i] <= (sValue[i][0] - sValue[i][1])) || (fileBufNew[i] >= (sValue[i][0] + sValue[i][1]))) {
-				if (DEBUG_PRINT)
-					printf("fileBufNew %i ist größer oder kleiner µ +- sigma\n");
-				statDif++;
-				sValue[i][0] = changeMy(count, fileBufNew[i], sValue[i][0]);
-			}
-			//fileBufOld[i] = fileBufNew[i];
-		}
-	}
-	//printf("sigma = %i\n", sigma);
-	count++;
-	return statDif;
-}
-
-int changeMy(int count, BYTE fileBufNew, int my) {
-	int myDif = 0;
-	// Check ob neuer Wert größer/kleiner my
-	if (my < fileBufNew)
-		myDif = fileBufNew - my;
-	else 
-		myDif = my - fileBufNew;
-
-	// Dividiere die Differenz durch count um bei einer Änderung nicht gleich total auszuschlagen.
-	// 10 als maximum gesetzt um nicht zu unsensibel zu Werten, wenn das Programm länger läuft.
-	if (count < 10)
-		myDif /= 10;
-	else
-		myDif /= 10;
-
-	// my neu setzen
-	if (my < fileBufNew)
-		my += myDif;
-	else
-		my -= myDif;
-	
-	return my;
+	return 0;
 }
 
 // Get the size of a file
@@ -453,197 +298,53 @@ long getFileSize(FILE *file)
 	return lEndPos;
 }
 
-void printImg(long fileSize, BYTE *fileBuf, int width) {
-	for (int i = 0; i < fileSize; i++) {
-		printf("%X ", fileBuf[i]);
-		if (i % width == 0 && i != 0)
-			printf("\n ");
-	}
-}
-
-void getKoordinates(long fileSize, BYTE *fileBuf) {
-	int x, y;	// Koordinatensystem
-	int image[WIDTH][HEIGHT][3]; // höhe, breite, pixel
-
-	// Index der Metadaten holen, da diese nicht gemappt werden sollen
-	int i = metadata(fileBuf);
-
-	// Zuweisung der r, g, b Werte auf die jeweiligen Koordinaten
-	for (x = 0; x < metad.width; x++) {
-		for (y = 0; y < metad.height; y++) {
-			image[x][y][0] = fileBuf[i++]; // r
-			image[x][y][1] = fileBuf[i++]; // g
-			image[x][y][2] = fileBuf[i++]; // b
-		}
-	}
-	// Kompremierung 
-
-}
-
-int metadata(BYTE *fileBuf) {
-	int i = 0;
-	if (PPM)
-		i = metappm(fileBuf, i);
-	if (PNM)
-		i = metapnm(fileBuf, i);
-	return i;
-}
-
-int metappm(BYTE *fileBuf, int i) {
-	int j = 0; // Zum zählen der newlines (4 in den Metadaten)
-	char width[10];
-	char height[10];
-	char color[10];
-	char *pwidth = width;
-	char *pheight = height;
-	char *pcolor = color;
-
-	if (!(fileBuf[0] == 'P' && fileBuf[1] == '6' && fileBuf[2] == '\n'))
-		return 0;
-	
-
-	// Check auf Kommentare
-	if (fileBuf[3] == '#') {
-		i = 4;
-		while (fileBuf[i] != '\n') {
-			i++;
-		}
-		i += 1;
-	}
-	// Check und speichern der Breite
-	while (fileBuf[i] != ' ') {
-		*(pwidth++) = fileBuf[i++];
-	}
-	*pwidth = '\0';
-	i += 1; // Da sonst auf leerzeichen
-	// Check und speichern der Höhe
-	while (fileBuf[i] != '\n') {
-		*(pheight++) = fileBuf[i++];
-	}
-	*pheight = '\0';
-	i += 1; // Da wir schon auf den newline stehen
-	// Check und speichern des Farbcodes
-	while (fileBuf[i] != '\n') {
-		*(pcolor++) = fileBuf[i++];
-	}
-	*pcolor = '\0';
-
-	metad.color = strtol(color, NULL, 10);
-	metad.height = strtol(height, NULL, 10);
-	metad.width = strtol(width, NULL, 10);
-
-	/*for (i; i < fileSize; i++) {
-
-	if (fileBuf[i] == 0x0a)
-	j++;
-	if (j == 4)
-	break;
-	}*/
-	return i + 1; // Da ich sonst auf \n stehe
-}
-
-int metapnm(BYTE *fileBuf, int i) {
+int metadata(FILE *fp) {
+	struct metadata metad;
 	// TODO: ERROR Glaube das Bildverzerren kommt weil wir nach der erste if nicht 3 mal hochzählen
-	int j = 0; // Zum zählen der newlines (4 in den Metadaten)
-	char width[10];
-	char height[10];
-	char color[10];
-	char *pwidth = width;
-	char *pheight = height;
-	char *pcolor = color;
 
-	if (!(fileBuf[0] == 'P' && fileBuf[1] == '5' && fileBuf[2] == '\n'))
-		return 0;
-	i = 3;
-	// Check auf Kommentare
-	if (fileBuf[3] == '#') {
-		i = 4;
-		while (fileBuf[i] != '\n') {
-			i++;
+	char magic[16];
+	char width[16];
+	char height[16];
+	char color[16];
+
+	char buffer[16];
+	char *pBuffer = buffer;
+
+	char *pState[4] = { magic, width, height, color };
+	int state = 0;
+
+	while ((fread(pBuffer, 1, 1, fp) != 0)) {
+		//comments
+		if (pBuffer == buffer && *pBuffer == '#') {
+			while (fread(buffer, 1, 1, fp) == 1) 
+				if (buffer[0] == '\n')
+					break;
+			
+			pBuffer = buffer;
+			continue;
 		}
-		i += 1;
+
+		if (*pBuffer == '\n' || *pBuffer == ' ') {
+			*pBuffer = '\0';
+
+			char *pActualState = pState[state];
+			for (int i = 0; i < sizeof(buffer) - 1; i++) {
+				pActualState[i] = buffer[i];
+			}
+
+			state++;
+			if (state >= 4) break;
+
+			pBuffer = buffer;
+			continue;
+		}
+
+		pBuffer++;
 	}
-	// Check und speichern der Breite
-	while (fileBuf[i] != ' ') {
-		*(pwidth++) = fileBuf[i++];
-	}
-	*pwidth = '\0';
-	i += 1; // Da sonst auf leerzeichen
-	// Check und speichern der Höhe
-	while (fileBuf[i] != '\n') {
-		*(pheight++) = fileBuf[i++];
-	}
-	*pheight = '\0';
-	i += 1; // Da wir schon auf den newline stehen
-	// Check und speichern des Farbcodes
-	while (fileBuf[i] != ' ') {
-		*(pcolor++) = fileBuf[i++];
-	}
-	*pcolor = '\0';
 
 	metad.color = strtol(color, NULL, 10);
 	metad.height = strtol(height, NULL, 10);
 	metad.width = strtol(width, NULL, 10);
 
-	/*for (i; i < fileSize; i++) {
-
-	if (fileBuf[i] == 0x0a)
-	j++;
-	if (j == 4)
-	break;
-	}*/
-	return i + 1; // Da ich sonst auf \n stehe
-}
-
-//union
-//	byteval {
-//	struct rgb_565 {
-//		int r : 5;
-//		int g : 6;
-//		int b : 5;
-//	}__attribute__(packed);
-//	struct rgb_888 {
-//		int r : 8;
-//		int g : 8;
-//		int b : 8;
-//	}__attribute__(packed);
-//};
-
-void convert(BYTE *frmBuffer) {
-	//BYTE r[] = (BYTE*)malloc(length / 3);
-	//BYTE g[] = (BYTE*)malloc(length / 3);
-	//BYTE b[] = (BYTE*)malloc(length / 3);
-
-	//for (int i = 0; i < length; i++) {
-	//	r[i] = ((((frmBuffer[i] >> 11) & 0x1F) * 527) + 23) >> 6;
-	//	g[i] = ((((frmBuffer[i] >> 5) & 0x3F) * 259) + 33) >> 6;
-	//	b[i] = ((((frmBuffer[i]) & 0x1F) * 527) + 23) >> 6;
-	//}
-
-	//for (int i = 0; i < length; i++) {
-	//	frmbuffer[i] = r[i] << 16 | g[i] << 8 | b;
-	//}
-
-
-	/*BYTE r, g, b;
-	for (int i = 0; i < length; i++) {
-		r = ((((frmBuffer[i] >> 11) & 0x1F) * 527) + 23) >> 6;
-		g = ((((frmBuffer[i] >> 5) & 0x3F) * 259) + 33) >> 6;
-		b = ((((frmBuffer[i]) & 0x1F) * 527) + 23) >> 6;
-
-		frmBuffer[i] = r << 16 | g << 8 | b;
-	}*/
-
-	int i = 0;
-	for (int y = 0; y < HEIGHT; y++) {
-		for (int x = 0; x < WIDTH; x++) {
-			uint16_t rgb = (frmBuffer[(y * WIDTH * 2) + x * 2 + 1] << 8)
-				| frmBuffer[(y * WIDTH * 2) + x * 2];
-			uint8_t r = (rgb & 0xF800) >> 8;
-			uint8_t g = (rgb & 0x07E0) >> 3;
-			uint8_t b = (rgb & 0x001F) << 3;
-			frmBuffer[i++] = (r << 11) | (g << 5) | (b);
-			frmBuffer[i++] = (r << 11) | (g << 5) | (b);
-		}
-	}
+	return ftell(fp);
 }
